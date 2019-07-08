c1269747ba970b9ec4ff52fd749088127d06124c
James Carr <james@jamescarr.dev>
2019-07-09 09:00:54 +1000

    chore: Update dependencies

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const withTypescript = require('@zeit/next-typescript'); // eslint-disable-line @typescript-eslint/no-var-requires
-
-module.exports = withTypescript({
-  /* Config options here */
-});
+module.exports = {
+  target: 'serverless',
+};
