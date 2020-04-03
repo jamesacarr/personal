0318592e113b201922286bc13444f343c68611d2
James Carr <james@jamescarr.dev>
2020-04-03 17:03:45 +1000

    feat: Use the @slack/webhook package to send messages

--- src/api/contact/generate-payload.ts
+++ src/api/contact/generate-payload.ts
@@ -1,35 +1,35 @@
+import { IncomingWebhookSendArguments } from '@slack/webhook';
+
 import { ContactRequestBody } from './types';
 
-const generatePayload = (body: ContactRequestBody): object => {
-  return {
-    text: 'You have a new message from the contact form:',
-    attachments: [
-      {
-        blocks: [
-          {
-            type: 'section',
-            fields: [
-              {
-                type: 'mrkdwn',
-                text: `*Name:*\n${body.name}`,
-              },
-              {
-                type: 'mrkdwn',
-                text: `*Email:*\n<mailto:${body.email}|${body.email}>`,
-              },
-            ],
-          },
-          {
-            type: 'section',
-            text: {
+const generatePayload = (body: ContactRequestBody): IncomingWebhookSendArguments => ({
+  text: 'You have a new message from the contact form:',
+  attachments: [
+    {
+      blocks: [
+        {
+          type: 'section',
+          fields: [
+            {
               type: 'mrkdwn',
-              text: `*Message:*\n${body.message}`,
+              text: `*Name:*\n${body.name}`,
             },
+            {
+              type: 'mrkdwn',
+              text: `*Email:*\n<mailto:${body.email}|${body.email}>`,
+            },
+          ],
+        },
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `*Message:*\n${body.message}`,
           },
-        ],
-      },
-    ],
-  };
-};
+        },
+      ],
+    },
+  ],
+});
 
 export default generatePayload;
--- src/api/contact/send-message.ts
+++ src/api/contact/send-message.ts
@@ -1,18 +1,21 @@
-import axios from 'axios';
+import { IncomingWebhook } from '@slack/webhook';
 import { INTERNAL_SERVER_ERROR } from 'http-status-codes';
 
 import errorHandler from '../error-handler';
 import generatePayload from './generate-payload';
 import { ContactRequest, ContactResponse } from './types';
 
+const url = process.env.SLACK_CONTACT_WEBHOOK ?? '';
+const webhook = new IncomingWebhook(url);
+
 const sendMessage = async (request: ContactRequest, response: ContactResponse): Promise<void> => {
-  const url = process.env.SLACK_CONTACT_WEBHOOK ?? '';
   if (!url) {
     errorHandler(response, INTERNAL_SERVER_ERROR);
     return;
   }
 
-  await axios.post(url, generatePayload(request.body));
+  const payload = generatePayload(request.body);
+  await webhook.send(payload);
   response.json({ success: true });
 };
 
