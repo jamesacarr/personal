98e5aec0cb8c8277e6f78bf4cb5bebe20baaf69b
James Carr <james@jamescarr.dev>
2022-02-21 19:35:01 +1000

    fix(contact): fix bug preventing contact api route from completing

--- src/api/contact/handler.ts
+++ src/api/contact/handler.ts
@@ -4,10 +4,11 @@ import sendMessage from './send-message';
 import validateBody from './validate-body';
 import validateMethod from './validate-method';
 
-const contactHandler: NextApiHandler<void> = async request => {
+const contactHandler: NextApiHandler<void> = async (request, response) => {
   validateMethod(request.method);
   const body = await validateBody(request.body);
   await sendMessage(body);
+  response.status(200).end();
 };
 
 export default contactHandler;
