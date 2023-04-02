4972ad047f1c6e2853ab7d25dd1cc675c5fe1c90
James Carr <james@jamescarr.dev>
2023-04-02 19:50:50 +1000

    ci: add typecheck to precomment hook

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,7 @@
+const typecheck = () => 'yarn typecheck';
+
 const config = {
-  '*.{js,jsx,ts,tsx}': ['yarn format', 'yarn lint'],
+  '*.{js,jsx,ts,tsx}': ['yarn format', 'yarn lint', typecheck],
   '*.{json,md,mdx,yml}': ['yarn format'],
 };
 
